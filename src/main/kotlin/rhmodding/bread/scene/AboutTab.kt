ef7cf3c442454dc0cb201fd8c54b4c20e95eab5b
chrislo27 <kinchrislo@gmail.com>
2019-07-27 19:06:34 +0800

    State license type

--- src/main/kotlin/rhmodding/bread/scene/AboutTab.kt
+++ src/main/kotlin/rhmodding/bread/scene/AboutTab.kt
@@ -14,7 +14,7 @@ class AboutTab(val app: Bread) : Tab("About") {
             styleClass += "vbox"
             alignment = Pos.CENTER
             children += Label("${Bread.TITLE} - A BRCAD editor\n${Bread.VERSION}\n\nDeveloped by chrislo27 and Malalaika\nIcon by garbo111")
-            // TODO put license
+            // TODO put Apache license
         }
     }
     
