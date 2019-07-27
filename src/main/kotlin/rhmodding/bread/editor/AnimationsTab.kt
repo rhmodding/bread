989aa1c8566d2a3389bb5835c1fc5e1bec9a79aa
chrislo27 <kinchrislo@gmail.com>
2019-07-27 11:31:32 +0800

    Add animation editing label

--- src/main/kotlin/rhmodding/bread/editor/AnimationsTab.kt
+++ src/main/kotlin/rhmodding/bread/editor/AnimationsTab.kt
@@ -1,6 +1,9 @@
 package rhmodding.bread.editor
 
+import javafx.geometry.Pos
+import javafx.scene.control.Label
 import javafx.scene.control.Tab
+import javafx.scene.layout.VBox
 import rhmodding.bread.model.IDataModel
 
 
@@ -9,6 +12,11 @@ open class AnimationsTab<F : IDataModel>(val editor: Editor<F>) : Tab("Animation
     protected val data: F get() = editor.data
 
     init {
+        content = VBox().apply {
+            styleClass += "vbox"
+            alignment = Pos.CENTER
+            children += Label("Animation editing not yet supported, sorry")
+        }
     }
 
 }
