d4f20c811a674232669d3ce6a5b82477465fa227
chrislo27 <kinchrislo@gmail.com>
2019-08-18 10:52:15 -0700

    Fix sprite part spinner being stuck at 0 for first sprite

--- src/main/kotlin/rhmodding/bread/util/Credits.kt
+++ src/main/kotlin/rhmodding/bread/util/Credits.kt
@@ -8,7 +8,8 @@ object Credits {
                 Credit("chrislo27", "https://github.com/chrislo27"),
                 Credit("@theonetruegarbo", "https://twitter.com/theonetruegarbo"),
                 Credit("Lvl100Feraligatr", "https://www.youtube.com/channel/UCigmB3cCbQ7wuY4SKHAL4WQ"),
-                Credit("meuol", "https://www.youtube.com/channel/UCNAUWWq3RKyGDHzBuKgEcPg")
+                Credit("meuol", "https://www.youtube.com/channel/UCNAUWWq3RKyGDHzBuKgEcPg"),
+                Credit("MF5K", "https://www.youtube.com/user/MrMariofan2020")
                      )
     }
     
