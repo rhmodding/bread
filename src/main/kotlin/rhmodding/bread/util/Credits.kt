a5354ee4fd9a8796045a34344f2fe495277296cc
chrislo27 <kinchrislo@gmail.com>
2019-09-01 22:24:43 -0700

    Update credits

--- src/main/kotlin/rhmodding/bread/util/Credits.kt
+++ src/main/kotlin/rhmodding/bread/util/Credits.kt
@@ -9,7 +9,8 @@ object Credits {
                 Credit("@theonetruegarbo", "https://twitter.com/theonetruegarbo"),
                 Credit("Lvl100Feraligatr", "https://www.youtube.com/channel/UCigmB3cCbQ7wuY4SKHAL4WQ"),
                 Credit("meuol", "https://www.youtube.com/channel/UCNAUWWq3RKyGDHzBuKgEcPg"),
-                Credit("MF5K", "https://www.youtube.com/user/MrMariofan2020")
+                Credit("MF5K", "https://www.youtube.com/user/MrMariofan2020"),
+                Credit("kimbjo", "https://github.com/oddMLan")
                      )
     }
     
