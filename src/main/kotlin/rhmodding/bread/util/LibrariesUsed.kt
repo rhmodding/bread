f3406aaf458c7aeb02758596f75039e6e4f5e84a
chrislo27 <kinchrislo@gmail.com>
2020-06-26 21:28:47 -0700

    Update libraries used

--- src/main/kotlin/rhmodding/bread/util/LibrariesUsed.kt
+++ src/main/kotlin/rhmodding/bread/util/LibrariesUsed.kt
@@ -7,6 +7,7 @@ object LibrariesUsed {
         Library("Kotlin", "https://kotlinlang.org/"),
         Library("bccad-editor", "https://github.com/rhmodding/bccad-editor"),
         Library("RHRE SFX Database Editor", "https://github.com/chrislo27/RSDE"),
-        Library("javafx-themes", "https://github.com/joffrey-bion/javafx-themes")
+        Library("Jackson", "https://github.com/FasterXML/jackson"),
+        Library("animated-gif-lib", "https://github.com/rtyley/animated-gif-lib-for-java")
     ).sortedBy { it.name }
 }
