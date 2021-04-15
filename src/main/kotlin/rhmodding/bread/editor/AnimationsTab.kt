99ff12828e3eef938cfaff663c08abe617090321
CebolaBros64 <15020325+CebolaBros64@users.noreply.github.com>
2021-04-15 11:32:31 -0300

    refactor duplicate code into a function

--- src/main/kotlin/rhmodding/bread/editor/AnimationsTab.kt
+++ src/main/kotlin/rhmodding/bread/editor/AnimationsTab.kt
@@ -37,7 +37,7 @@ import kotlin.math.roundToInt
 
 
 open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(editor, "Animations") {
-    
+
     val body: VBox = VBox().apply {
         isFillWidth = true
     }
@@ -45,7 +45,7 @@ open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(ed
     val stepPropertiesVBox: VBox = VBox().apply {
         disableProperty().bind(disableStepControls)
     }
-    
+
     val animationSpinner: Spinner<Int> = intSpinnerFactory(0, data.animations.size - 1, 0).spinnerArrowKeysAndScroll()
     val aniStepSpinner: Spinner<Int> = intSpinnerFactory(0, currentAnimation.steps.size - 1, 0).spinnerArrowKeysAndScroll()
     val playStopButton: Button = Button("Play")
@@ -59,15 +59,15 @@ open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(ed
     val stepStretchYSpinner: Spinner<Double> = doubleSpinnerFactory(-Float.MAX_VALUE.toDouble(), Float.MAX_VALUE.toDouble(), 1.0, 0.1).spinnerArrowKeysAndScroll()
     val stepRotationSpinner: Spinner<Double> = doubleSpinnerFactory(-Double.MAX_VALUE, Double.MAX_VALUE, 0.0, 0.1).spinnerArrowKeysAndScroll()
     val stepOpacitySpinner: Spinner<Int> = intSpinnerFactory(0, 255, 255).spinnerArrowKeysAndScroll()
-    
+
     val numAnimationsLabel: Label = Label("")
     val numAniStepsLabel: Label = Label("")
-    
+
     val currentAnimation: IAnimation
         get() = data.animations[animationSpinner.value]
     val currentAnimationStep: IAnimationStep
         get() = currentAnimation.steps[aniStepSpinner.value]
-    
+
     var currentTimeline: ObjectProperty<Timeline?> = SimpleObjectProperty(null as Timeline?).apply {
         addListener { _, old, new ->
             old?.stop()
@@ -75,15 +75,15 @@ open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(ed
         }
     }
     val playbackStepProperty: SimpleIntegerProperty = SimpleIntegerProperty(0)
-    
+
     val sectionAnimation: VBox
-    
+
     init {
         content = ScrollPane(body).apply {
             this.hbarPolicy = ScrollPane.ScrollBarPolicy.AS_NEEDED
             this.vbarPolicy = ScrollPane.ScrollBarPolicy.AS_NEEDED
         }
-        
+
         animationSpinner.valueProperty().addListener { _, _, _ ->
             this@AnimationsTab.editor.repaintCanvas()
             (aniStepSpinner.valueFactory as SpinnerValueFactory.IntegerSpinnerValueFactory).also {
@@ -110,7 +110,7 @@ open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(ed
         sectionAnimation = VBox().apply {
             styleClass += "vbox"
             alignment = Pos.CENTER_LEFT
-            
+
             children += HBox().apply {
                 styleClass += "hbox"
                 alignment = Pos.CENTER_LEFT
@@ -132,7 +132,7 @@ open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(ed
             children += HBox().apply {
                 styleClass += "hbox"
                 alignment = Pos.CENTER_LEFT
-                
+
                 fun updateStepSpinners(goToMax: Boolean) {
                     (aniStepSpinner.valueFactory as SpinnerValueFactory.IntegerSpinnerValueFactory).also {
                         it.max = (currentAnimation.steps.size - 1).coerceAtLeast(0)
@@ -141,7 +141,7 @@ open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(ed
                     updateFieldsForStep()
                     editor.repaintCanvas()
                 }
-                
+
                 children += Button("Add New Step").apply {
                     setOnAction {
                         editor.addAnimationStep(currentAnimation, editor.createAnimationStep())
@@ -194,22 +194,11 @@ open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(ed
 
                         val file = fileChooser.showSaveDialog(null)
                         if (file != null) {
-                            val canvas = editor.canvas
-                            val showGrid = editor.showGridCheckbox.isSelected
-
-                            val writableImage = WritableImage(canvas.width.toInt(), canvas.height.toInt())
-
-                            editor.drawCheckerBackground(canvas, showGrid = showGrid, darkGrid = false)
-                            editor.drawAnimationStep(ani.steps[curStep])
-
-                            var sp: SnapshotParameters = SnapshotParameters()
-                            sp.setFill(Color.TRANSPARENT)
-                            canvas.snapshot(sp, writableImage)
+                            var writableImage = writeTransparentSnapshot(ani.steps[curStep])
 
                             ImageIO.write(SwingFXUtils.fromFXImage(writableImage, null), "png", file);
-
-                            editor.repaintCanvas()
                         }
+                        editor.repaintCanvas()
                     }
                 }
                 children += Button("Export All Steps").apply {
@@ -224,20 +213,10 @@ open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(ed
                         if (dir != null) {
                             var file: File
                             val padCount: Int = ani.steps.size.toString().length.coerceAtLeast(3)
+
                             ani.steps.forEachIndexed { curStep, step ->
                                 val stepNum: String = curStep.toString().padStart(padCount, padChar = '0')
-
-                                val canvas = editor.canvas
-                                val showGrid = editor.showGridCheckbox.isSelected
-
-                                val writableImage = WritableImage(canvas.width.toInt(), canvas.height.toInt())
-
-                                editor.drawCheckerBackground(canvas, showGrid = showGrid, darkGrid = false)
-                                editor.drawAnimationStep(step)
-
-                                var sp: SnapshotParameters = SnapshotParameters()
-                                sp.setFill(Color.TRANSPARENT)
-                                canvas.snapshot(sp, writableImage)
+                                var writableImage = writeTransparentSnapshot(step)
 
                                 file = dir.resolve("${getAnimationNameForGifExport()}.$stepNum.png")
                                 ImageIO.write(SwingFXUtils.fromFXImage(writableImage, null), "png", file);
@@ -303,11 +282,11 @@ open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(ed
         }
         body.children += stepPropertiesVBox.apply {
             alignment = Pos.CENTER_LEFT
-            
+
             children += TitledPane("Playback", VBox().apply {
                 styleClass += "vbox"
                 alignment = Pos.CENTER_LEFT
-                
+
                 children += HBox().apply {
                     styleClass += "hbox"
                     alignment = Pos.CENTER_LEFT
@@ -410,20 +389,10 @@ open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(ed
                             if (dir != null) {
                                 var file: File
                                 val padCount: Int = ani.steps.size.toString().length.coerceAtLeast(3)
+
                                 ani.steps.forEachIndexed { curStep, step ->
                                     val stepNum: String = curStep.toString().padStart(padCount, padChar = '0')
-
-                                    val canvas = editor.canvas
-                                    val showGrid = editor.showGridCheckbox.isSelected
-
-                                    val writableImage = WritableImage(canvas.width.toInt(), canvas.height.toInt())
-
-                                    editor.drawCheckerBackground(canvas, showGrid = showGrid, darkGrid = false)
-                                    editor.drawAnimationStep(step)
-
-                                    var sp: SnapshotParameters = SnapshotParameters()
-                                    sp.setFill(Color.TRANSPARENT)
-                                    canvas.snapshot(sp, writableImage)
+                                    var writableImage = writeTransparentSnapshot(step)
 
                                     repeat(step.delay.toInt()) { i ->
                                         file = dir.resolve("${getAnimationNameForGifExport()}.$stepNum.${i}.png")
@@ -438,33 +407,33 @@ open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(ed
             }).apply {
                 styleClass += "titled-pane"
             }
-            
+
             children += TitledPane("Step Properties", VBox().apply {
                 styleClass += "vbox"
                 alignment = Pos.CENTER_LEFT
-                
+
                 children += GridPane().apply {
                     styleClass += "grid-pane"
                     alignment = Pos.CENTER_LEFT
-                    
+
                     add(Label("Sprite Index:"), 0, 0)
                     add(stepSpriteSpinner, 1, 0)
-                    
+
                     add(Label("Delay:"), 0, 1)
                     add(stepDelaySpinner, 1, 1)
                     add(Label("frames"), 2, 1)
-                    
+
                     add(Label("Scale X:"), 0, 2)
                     add(stepStretchXSpinner, 1, 2)
                     add(Label("Y:").apply {
                         GridPane.setHalignment(this, HPos.RIGHT)
                     }, 2, 2)
                     add(stepStretchYSpinner, 3, 2)
-                    
+
                     add(Label("Rotation:"), 0, 3)
                     add(stepRotationSpinner, 1, 3)
                     add(Label("${Typography.degree}"), 2, 3)
-                    
+
                     add(Label("Opacity:"), 0, 4)
                     add(stepOpacitySpinner, 1, 4)
                 }
@@ -472,12 +441,12 @@ open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(ed
                 styleClass += "titled-pane"
             }
         }
-        
+
         Platform.runLater {
             updateFieldsForStep()
         }
     }
-    
+
     open fun updateFieldsForStep() {
         numAnimationsLabel.text = "(${data.animations.size} total animation${if (data.animations.size == 1) "" else "s"})"
         numAniStepsLabel.text = "(${currentAnimation.steps.size} total step${if (currentAnimation.steps.size == 1) "" else "s"})"
@@ -487,7 +456,7 @@ open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(ed
         }
         val step = currentAnimationStep
         disableStepControls.value = false
-        
+
         stepSpriteSpinner.valueFactoryProperty().get().value = step.spriteIndex.toInt()
         stepDelaySpinner.valueFactoryProperty().get().value = step.delay.toInt()
         stepStretchXSpinner.valueFactoryProperty().get().value = step.stretchX.toDouble()
@@ -506,9 +475,24 @@ open class AnimationsTab<F : IDataModel>(editor: Editor<F>) : EditorSubTab<F>(ed
             this.value = aniStepSpinner.value.toDouble()
         }
     }
-    
+
     protected open fun getAnimationNameForGifExport(): String {
         return "animation_${animationSpinner.value}"
     }
-    
+
+    protected open fun writeTransparentSnapshot(step: IAnimationStep): WritableImage {
+        val canvas = editor.canvas
+        val showGrid = editor.showGridCheckbox.isSelected
+        val writableImage = WritableImage(canvas.width.toInt(), canvas.height.toInt())
+
+        editor.drawCheckerBackground(canvas, showGrid = showGrid, darkGrid = false)
+        editor.drawAnimationStep(step)
+
+        val sp: SnapshotParameters = SnapshotParameters()
+        sp.setFill(Color.TRANSPARENT)
+        canvas.snapshot(sp, writableImage)
+
+        return writableImage
+    }
+
 }
