a2604e7da6711b3a466782441eeeeb752528d885
CebolaBros64 <15020325+CebolaBros64@users.noreply.github.com>
2021-04-15 12:12:47 -0300

    Update credits

--- src/main/kotlin/rhmodding/bread/util/Credits.kt
+++ src/main/kotlin/rhmodding/bread/util/Credits.kt
@@ -10,7 +10,8 @@ object Credits {
                 Credit("Lvl100Feraligatr", "https://www.youtube.com/channel/UCigmB3cCbQ7wuY4SKHAL4WQ"),
                 Credit("meuol", "https://www.youtube.com/channel/UCNAUWWq3RKyGDHzBuKgEcPg"),
                 Credit("MF5K", "https://www.youtube.com/user/MrMariofan2020"),
-                Credit("kimbjo", "https://github.com/oddMLan")
+                Credit("kimbjo", "https://github.com/oddMLan"),
+                Credit("CebolaBros64", "https://github.com/CebolaBros64")
                      )
     }
     
